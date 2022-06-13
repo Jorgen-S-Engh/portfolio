3771efd3130c7792a4f2d0df92f019f400419198
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-06-13 14:13:06 +0200

    did changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 const contact = document.querySelector(".contact-info");
 const button = document.querySelector(".contact-button");
+const skillIcon = document.querySelector(".skill-icons");
 const downloadIcon = document.querySelector(".fa-circle-arrow-down");
 const downloadButton = document.querySelector(".cv-button");
+const age = document.querySelector(".my-age");
+// const showMore = document.querySelector(".show-more");
+// const thirdRow = document.querySelector(".third-row");
 
 button.addEventListener("click", () => {
   contact.style.display = "flex";
-  contact.syle.visibility = "visible";
-  contact.style.transition = "all 2s";
   button.style.display = "none";
 });
 
@@ -19,3 +21,17 @@ downloadButton.addEventListener("mouseout", () => {
   downloadIcon.style.color = "#f5f5f5";
   downloadButton.style.color = "#f5f5f5";
 });
+
+// showMore.addEventListener("click", () => {
+//   thirdRow.style.display = "flex";
+//   showMore.innerHTML = "Show less";
+// });
+
+// AGE FUNCTION
+function calculateAge(bday) {
+  const dateDiff = Date.now() - bday.getTime();
+  const ageDt = new Date(dateDiff);
+  return Math.abs(ageDt.getUTCFullYear() - 1970);
+}
+const myAge = calculateAge(new Date(1988, 7, 13));
+age.innerHTML = myAge;
