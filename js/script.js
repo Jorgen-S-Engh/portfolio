51d9be56ed6ffa1655451e09d5d246e86e01fe0d
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2023-01-23 09:23:32 +0100

    add projects

--- js/script.js
+++ js/script.js
@@ -56,3 +56,70 @@ window.onclick = (e) => {
     modalContainer.style.display = "none";
   }
 };
+
+//Get posts
+
+const portfolioContainer = document.querySelector(".portfolio");
+
+async function getPosts() {
+  const reply = await fetch("https://www.jorgenpress.no/wp-json/wp/v2/posts/");
+  const data = await reply.json();
+  for (let i = 0; i < data.length; i++) {
+    // console.log(data[i].excerpt.rendered);
+    portfolioContainer.innerHTML += `
+                                        <div class="portfolio-item">
+                                        <div class="portfolio-headings">
+                                          <p class="portfolio-number">0${
+                                            i + 1
+                                          }</p>
+                                          <h3>${data[i].title.rendered}</h3>
+                                          <hr />
+                                        </div>
+                                        <a class="portfolio-img-container" href="" target="_blank">
+                                          <img src="${
+                                            data[i].featured_media_src_url
+                                          }" alt="Picture of the webpage ${
+      data[i].title.rendered
+    }" />
+                                        </a>
+                                        <div class="portfolio-card">
+                                          <h3>Description:</h3>
+                                          ${data[i].content.rendered}
+
+                                          <a href="${
+                                            data[i].excerpt.rendered
+                                          }" target="_blank"><i
+                                              class="fa-brands fa-github"></i></a>
+                                          <h4>Built with:</h4>
+                                          <div class="built-with">
+                                            <div>
+                                              <i class="fa-brands fa-html5"></i>
+                                              <p>HTML5</p>
+                                            </div>
+                                            <div>
+                                              <i class="fa-brands fa-bootstrap"></i>
+                                              <p>Bootstrap</p>
+                                            </div>
+                                            <div>
+                                              <i class="fa-brands fa-sass"></i>
+                                              <p>SASS</p>
+                                            </div>
+                                            <div>
+                                              <i class="fa-brands fa-js-square"></i>
+                                              <p>Js</p>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+  
+  `;
+  }
+}
+
+function trim(str, size) {
+  if (str.length > 14) {
+  }
+  console.log(str.length);
+}
+
+trim("Creating kata is fun", 14);
